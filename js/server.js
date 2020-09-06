a4df057776c0166dbfacefd98335bc3f5a98f373
Haechi <combuff@nethru.co.kr>
2020-09-06 22:10:26 +0900

    웹서버구축

---
+++ js/server.js
@@ -0,0 +1,55 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(request, response) {
+	console.log("요청 URL", request.url);
+		
+	var str = getstr(request.url);
+		
+	if(isText(str))	{
+		fs.readFile(str, "utf-8", function(error, data) {
+			content(data);
+		});
+	}
+	else {
+		fs.readFile(str, function(error, data) {
+			console.log(error);
+			content(data);
+		});
+	}
+	
+	function content(data) {
+		response.writeHead(200, {
+			"content-type": getContentType(str) + (isText(str) ? "; charset=utf-8" : "")
+		});
+			
+		response.end(data);
+	}
+});
+
+server.listen(8888);
+
+console.log("나 듣고 있다!");
+
+function getstr(request) {
+	if(request == "/") return "yacht.html";
+	
+	return request.substr(1, request.length - 1);
+}
+
+function getContentType(str) {
+	var arr = str.split(".");
+	
+	var extension = arr[arr.length - 1];
+	
+	if(extension == "html") return "text/html";
+	else if(extension == "css") return "text/css";
+	else if(extension == "js") return "application/javascript";
+	else if(extension == "png") return "image/png";
+	else if(extension == "ico") return "image/x-icon";
+	else if(extension == "m4a") return "audio/mp4";
+}
+
+function isText(str) {
+	return getContentType(str) == "text/html" || getContentType(str) == "text/css" || getContentType(str) == "application/javascript";
+}
