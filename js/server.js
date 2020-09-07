cb251ad78ec168b4d16769cdd6a6b2ed523d12cb
Haechi <combuff@nethru.co.kr>
2020-09-07 11:15:33 +0900

    mime 패키지를 사용할수 있도록 수정

--- js/server.js
+++ js/server.js
@@ -1,26 +1,28 @@
 var http = require("http");
 var fs = require("fs");
+var mime = require("mime");
 
 var server = http.createServer(function(request, response) {
-	console.log("요청 URL", request.url);
+	var filepath = getfilepath(request.url);
+	var contentType = mime.getType(filepath);
+	
+	console.log("요청 URL: " + request.url + " " + contentType);
 		
-	var str = getstr(request.url);
-		
-	if(isText(str))	{
-		fs.readFile(str, "utf-8", function(error, data) {
+	if(isText(contentType))	{
+		fs.readFile(filepath, "utf-8", function(error, data) {
 			content(data);
 		});
 	}
 	else {
-		fs.readFile(str, function(error, data) {
+		fs.readFile(filepath, function(error, data) {
 			content(data);
 		});
 	}
 	
 	function content(data) {
 		response.writeHead(200, {
-			"content-type": getContentType(str) + (isText(str) ? "; charset=utf-8" : ""),
-			"cache-control": isText(str) ? "no-cache" : "max-age=31536000"
+			"content-type": contentType + (isText(contentType) ? "; charset=utf-8" : ""),
+			"cache-control": isText(contentType) ? "no-cache" : "max-age=31536000"
 		});
 			
 		response.end(data);
@@ -31,25 +33,12 @@ server.listen(8888);
 
 console.log("나 듣고 있다!");
 
-function getstr(request) {
+function getfilepath(request) {
 	if(request == "/") return "yacht.html";
 	
 	return request.substr(1, request.length - 1);
 }
 
-function getContentType(str) {
-	var arr = str.split(".");
-	
-	var extension = arr[arr.length - 1];
-	
-	if(extension == "html") return "text/html";
-	else if(extension == "css") return "text/css";
-	else if(extension == "js") return "application/javascript";
-	else if(extension == "png") return "image/png";
-	else if(extension == "ico") return "image/x-icon";
-	else if(extension == "m4a") return "audio/mp4";
-}
-
-function isText(str) {
-	return getContentType(str) == "text/html" || getContentType(str) == "text/css" || getContentType(str) == "application/javascript";
+function isText(contentType) {
+	return contentType == "text/html" || contentType == "text/css" || contentType == "application/javascript";
 }
